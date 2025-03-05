8a3d2cc9111db0fd9f8c4ac02cadcd2132da380e
rehan-rh <rehanhussain0842@gmail.com>
2025-03-06 00:59:03 +0530

    giff aligned

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+
 const Hero = () => {
   return (
     <section
       id="hero"
-      className="relative min-h-screen bg-cover bg-center bg-no-repeat"
+      className="relative min-h-screen flex items-center bg-cover bg-center bg-no-repeat"
       style={{
         backgroundImage:
           "url('https://source.unsplash.com/1600x900/?technology,developer')",
@@ -14,9 +15,10 @@ const Hero = () => {
       <div className="absolute inset-0 bg-black bg-opacity-50"></div>
 
       {/* Content */}
-      <div className="relative z-10 flex flex-col md:flex-row items-center justify-between min-h-screen text-white px-6 md:px-16">
+      <div className="relative z-10 flex flex-col md:flex-row items-center justify-between w-full max-w-6xl mx-auto px-6 md:px-16 gap-x-12">
+        
         {/* Left Section - Animated Text */}
-        <div className="text-center md:text-left md:w-1/2">
+        <div className="text-center md:text-left md:w-1/2 mt-2">
           <h1 className="text-5xl md:text-6xl font-extrabold leading-tight mb-6 animate-fadeInUp">
             Hi, I'm <span className="text-blue-400">Rehan</span>
             <br />
@@ -48,8 +50,8 @@ const Hero = () => {
         </div>
 
         {/* Right Section - Full Stack GIF */}
-        <div className="mt-12 md:mt-0 md:w-1/2 flex justify-center items-center">
-          <div className="w-80 h-80 rounded-xl overflow-hidden shadow-lg">
+        <div className="md:w-1/2 flex justify-center md:justify-end items-center">
+          <div className="w-[450px] h-[450px] rounded-xl overflow-hidden shadow-lg mb-3">
             <DotLottieReact
               src="https://lottie.host/6cea878b-f539-4aa9-819a-0a9ad2c7bd55/2RV4FXEfKm.lottie"
               loop
@@ -57,6 +59,7 @@ const Hero = () => {
             />
           </div>
         </div>
+        
       </div>
     </section>
   );
